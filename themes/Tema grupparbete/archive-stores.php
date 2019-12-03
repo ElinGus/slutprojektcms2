d0bddfa220c013c43f471ee6b8ea865327b061ea
Annika <karlsson.e.annika@gmail.com>
2019-12-03 15:25:41 +0100

    update store locator

---
+++ themes/Tema grupparbete/archive-stores.php
@@ -0,0 +1,31 @@
+<!-- Annika -->
+
+<?php get_header();
+get_template_part('partials/navbar');?>
+
+<!-- Slider med Bulma Carousel -->
+<!-- Valde en slider här istället för klassisk blogginläggslook då vi ville ha
+det lite renare och mer lättöverskådligt. Det är en vanlig loop som hämtar
+poster från custom post type 'Tjänster/services' där man givetvis kan redigera
+och lägga till innehåll i wp-admin. Hade lite bekymmer med att få rätt storlek
+på den men laborerade mig fram till det via Bulmas dokumentation -->
+<section>
+    <div class="container">
+      <div class="columns">
+        <div class="column is-three-fifths is-offset-one-fifth">
+        <h1>Store locator</h1>
+              <?php
+                while (have_posts()) {
+                    the_post();
+                    //the_post_thumbnail_url();
+                    ?>
+                    <p><a href="<?php the_permalink(); ?>"><?php the_title(); ?> &nbsp; <i class="fa fa-angle-double-right"></i></a></p>
+                    <?php
+                  }
+                  ?>
+        </div>
+      </div>
+    </div>              
+</section>
+
+<?php get_footer(); ?>
--- themes/Tema grupparbete/single-stores.php
+++ themes/Tema grupparbete/single-stores.php
@@ -1,1 +1,38 @@
-<h1>test</h1>
+<!-- Annika -->
+
+<?php get_header();
+get_template_part('partials/navbar');?>
+
+<div class="container">
+  <div class="columns">
+    <div class="column is-three-fifths is-offset-one-fifth">
+      <?php
+        if (have_posts()) {
+    	   while (have_posts()) {
+    		     the_post();
+             ?>
+              <article class="blogarticle">
+                <img src=<?php the_post_thumbnail_url(); ?> />
+                <p class="title">
+                  <?php the_title(); ?>
+                </p>
+                <p> <?php the_content(); ?></p>
+                <?php while (have_rows('address_and_map')) {
+                    the_row();
+                      if (get_row_layout() == 'address&map') {?>
+                        <p> <?php the_sub_field('address');?> </p>
+                        <p> <?php echo do_shortcode(get_sub_field('map')); ?> </p>
+                        <?php
+                      }
+                  }
+                  ?>
+              </article>
+        <?php
+           }
+        }
+         ?>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
